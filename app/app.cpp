787075851fb8bc2a62a7d2c0ba09e78355bee545
breski-igor <igor.breski@vsite.hr>
2025-01-16 08:18:16 +0100

    Revert "gotovo"

--- app/app.cpp
+++ app/app.cpp
@@ -1,43 +1,6 @@
 #include "app.h"
-#include <string>
-#include <iostream>
-using namespace std;
 
 namespace vsite::oop::v8
 {
-	int input_num(std::istream& input) {
-		int n;
-		input >> n;
-		if (input.fail()) {
-			throw not_number();
-		}
-		return n;
-	}
 
-	char input_op(std::istream& input) {
-		char c;
-		input >> c;
-		if ("+-*/"s.find(c) == std::string::npos) {
-			throw not_operator();
-		}
-		return c;
-	}
-
-	double calc(double x, char c, double y) {
-		switch (c) {
-		case '+':
-			return x + y;
-		case '-':
-			return x - y;
-		case '*':
-			return x * y;
-		case '/':
-			if (y == 0){ 
-				throw divide_zero();
-			}
-			else {
-				return x / y;
-			}
-		}
-	}
 }
